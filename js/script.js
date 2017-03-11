67e519e240c08c1fa8cf1331cda7d411e1369a4a
Joel Watson <existdissolve@gmail.com>
2017-03-10 21:46:25 -0600

    Updates

--- js/script.js
+++ js/script.js
@@ -8,8 +8,8 @@ http://www.andreagalanti.it
 
 $(window).load(function() { 
 	//Preloader 
-	$('#status').delay(300).fadeOut(); 
-	$('#preloader').delay(300).fadeOut('slow');
+	//$('#status').delay(300).fadeOut(); 
+	//$('#preloader').delay(300).fadeOut('slow');
 	$('body').delay(550).css({'overflow':'visible'});
 })
 
